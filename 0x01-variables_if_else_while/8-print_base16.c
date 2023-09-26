ccecf62ac23f87806e579d12fb0e69f1d5d2665d
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2023-09-26 10:35:59 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,20 @@
-#include <stdio.h> 
- /** 
-  * main - start point of the program 
-  * 
-  * Return: always must be 0 in case you are using int if you using void 
-  * 
-  *  no need to return keyword 
-  */ 
- int main(void) 
- { 
- /* initialize a counter that hold the value of number as char */ 
- /* very important note we can not use counter<=10 */ 
- /* because putchar take only one char and if i give it 2 char */ 
- /* leads to a buffer overflow so be carfule when using putchar to hold */ 
- /* data from user's input */ 
- int counter; 
- for (counter = '0'; counter <= '9'; counter++) 
- { 
- putchar(counter); 
- } 
- for (counter = 'a'; counter <= 'f'; counter++) 
- { 
- putchar(counter); 
- } 
- putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+* main - start point of the program
+* Return: always must be 0 in case you are using int if you using void
+* no need to return keyword
+*/
+int main(void)
+{
+int counter;
+for (counter = '0'; counter <= '9'; counter++)
+{
+putchar(counter);
+}
+for (counter = 'a'; counter <= 'f'; counter++)
+{
+putchar(counter);
+}
+putchar('\n');
+return (0);
+}
