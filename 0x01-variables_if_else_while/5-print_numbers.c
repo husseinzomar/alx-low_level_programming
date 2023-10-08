dee4e4ceeb0421b5f451b7decabe957d61a069b5
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2023-10-08 12:26:13 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,16 @@
-#include <stdio.h> 
- /** 
-  * main - start point of the program 
-  * 
-  * Return: always must be 0 in case you are using int if you using void 
-  * 
-  *  no need to return keyword 
-  */ 
- int main(void) 
- { 
- /* initialize a counter that hold the value of number*/ 
- int counter; 
- for (counter = 0; counter < 10; counter++) 
- { 
- printf("%d", counter); 
- } 
- putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+* main - start point of the program
+* Return: always must be 0 in case you are using int if you using void
+*  no need to return keyword
+*/
+int main(void)
+{
+int counter;
+for (counter = 0; counter < 10; counter++)
+{
+printf("%d", counter);
+}
+putchar('\n');
+return (0);
+}
