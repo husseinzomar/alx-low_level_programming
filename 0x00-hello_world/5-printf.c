55235b4607c411cb92b05367b2cfe4024e970eca
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2023-10-08 12:32:34 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-        puts("\"programming is like building a multilingual puzzle");
-	return (0);	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
