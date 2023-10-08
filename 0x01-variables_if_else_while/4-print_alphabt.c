fa9ae40490be6927327923dc55b3aae86a212022
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2023-10-08 12:27:48 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,20 @@
-#include <stdio.h> 
- /** 
-  * main - start point of the program 
-  * 
-  * Return: always must be 0 in case you are using int if you using void 
-  * 
-  *  no need to return keyword 
-  */ 
- int main(void) 
- { 
- /* initialize a counter that hold the value of char as 1=a ,2=b and so on*/ 
- int n; 
- for (n = 'a'; n <= 'z'; n++) 
- { 
- /* ignor both of e and q by using contiue keyword */ 
- /* performing next struction*/ 
- if (n != 'e' && n != 'q') 
- { 
- putchar(n); 
- continue; 
- } 
- } 
- putchar ('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+* main - start point of the program
+* Return: always must be 0 in case you are using int if you using void
+* no need to return keyword
+*/
+int main(void)
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+if (n != 'e' && n != 'q')
+{
+putchar(n);
+continue;
+}
+}
+putchar('\n');
+return (0);
+}
