9d51294c9576175a1c711efa4194427f49179501
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2023-11-08 20:43:05 +0200

    new

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argv: vector array
+ * @argc: length
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int first_oper, second_oper;
+
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	first_oper = atoi(argv[1]);
+	second_oper = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operation(first_oper, second_oper));
+	return (0);
+}
